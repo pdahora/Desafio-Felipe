338066b1378280f9173c574bc4b01f00cd4ae4c1
pdahora <pdahora@hotmail.com>
2023-09-28 12:17:52 -0300

    nova ação

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let nome = "paulo"
-console.log(nome)
+let nome = "Valente"
+let exp = 1000
+
+if (exp == 1000){
+    console.log ("valente é nível bronze")
+}
+else if (exp >1010 && exp <= 2000){
+    console.log ("valente é nível prata")
+}
+else
+    console.log ("valente é nível muito baixo")
 
