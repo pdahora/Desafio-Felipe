5cfe0b60f7b15d20904b8dfb188be7a8f9e3783e
pdahora <pdahora@hotmail.com>
2023-09-28 15:37:27 -0300

    Quarto teste

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-let nome = "Valente"
-let exp = 1000
+function Classificacao(nomeJogador, exp) {
+    if (exp == 1000) {
+        return nomeJogador + ", seu nível é: bronze"
+    }
+    else if (exp > 1010 && exp <= 2000) {
+        return nomeJogador + ", seu  nível é: prata"
+    }
+    else
+        return nomeJogador + ", seu  nível é: baixo"
 
-if (exp == 1000){
-    console.log ("valente é nível bronze")
 }
-else if (exp >1010 && exp <= 2000){
-    console.log ("valente é nível prata")
-}
-else
-    console.log ("valente é nível muito baixo")
+let nomeJogador = "Valente";
+let exp = 1350;
+let classificacao = Classificacao(nomeJogador, exp);
 
+console.log(classificacao);
