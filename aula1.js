c73388a6349a8afe492547809e114ac2c86835c0
pdahora <pdahora@hotmail.com>
2023-09-28 16:31:39 -0300

    Primeira aula em Switch Case

---
+++ aula1.js
@@ -0,0 +1,16 @@
+
+let aeroporto = "Nat"
+
+switch(aeroporto){
+    
+    case "Rec":
+        console.log("Recife")
+        break
+    case "Nat":
+        console.log("Natal")  
+        break 
+    case "Mco":
+        console.log("Maceio")
+        break    
+}
+console.log(aeroporto)
